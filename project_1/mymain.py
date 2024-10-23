6a367820258f3540b273c05594f9a7dfefd24577
Ari Saha <asaha@live.com>
2024-10-23 04:36:19 -0500

    Tree model is working

--- project_1/mymain.py
+++ project_1/mymain.py
@@ -1,132 +1,119 @@
+from os.path import join, dirname
+import time
+
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.linear_model import Lasso, Ridge
-from sklearn.metrics import mean_squared_error
-import numpy as np
-import warnings
-# Suppress only the SettingWithCopyWarning
-warnings.filterwarnings('ignore', category=pd.errors.SettingWithCopyWarning)
+from sklearn.linear_model import Ridge
+from xgboost import XGBRegressor
 
-def load_data(train_file: str, test_file: str):
-    train_data = pd.read_csv(train_file)
-    test_data = pd.read_csv(test_file)
-    return train_data, test_data
 
-def clean_and_process_categorical_features(train, test):
-
-    original_columns = train.columns
-    # impute missing categories with the mode
-    for col in original_columns:
-        train.loc[:, col] = train[col].fillna(train[col].mode()[0])
-        test.loc[:, col] = test[col].fillna(test[col].mode()[0])
-
-    encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
-    encoder.fit(train)
-    
-    encoded_train = encoder.transform(train)
-    encoded_test = encoder.transform(test)
-    
-    encoded_train_df = pd.DataFrame(encoded_train, columns=encoder.get_feature_names_out(original_columns))
-    encoded_test_df = pd.DataFrame(encoded_test, columns=encoder.get_feature_names_out(original_columns))
-    return encoded_train_df, encoded_test_df
-
-def clean_and_process_numeric_features(data):
-    # impute missing values with median
-    for col in data.columns:
-        data.loc[:, col] = data[col].fillna(data[col].median())
-
-    cols = data.columns
-    
-    # standardize the features
-    scaler = StandardScaler()
-    data = scaler.fit_transform(data)
-    return pd.DataFrame(data, columns = cols)
-   
-def add_feature_engineered_cols(data):
-    
-    # add some features
-    data.loc[:, 'Total_SF'] = data[['First_Flr_SF','Second_Flr_SF','Total_Bsmt_SF']].copy().sum(axis=1)
-    data.loc[:, 'Total_Bath'] = data.loc[:,'Full_Bath'] + (0.5 * data.loc[:,'Half_Bath'])
-    data['Has_Pool'] = data['Pool_QC'].map({"No_Pool":0, 'Excellent':1, 'Good':1, 'Typical':0, 'Fair':0})
-    data['Has_Quality_Kitchen'] = data['Kitchen_Qual'].map({'Excellent':1, 'Good':1, 'Typical':0, 'Fair':0, 'Poor':0})
-    data['Has_Quality_Basement'] = data['Bsmt_Qual'].map({'Excellent':1, 'Good':1, 'Typical':0, 'Fair':0, 'Poor':0})
-
-    # drop some of the cols
-    data = data.drop(columns=['Bsmt_Qual', 'Kitchen_Qual', 'Pool_QC'])
-
-    return data
-    
-def preprocess_data(train: pd.DataFrame, test: pd.DataFrame):
-
-    X_train = train.drop(columns=["Sale_Price", "PID"])
-    y_train = train.Sale_Price.apply(np.log)
-
-    X_test = test.drop(columns=["PID"])
-
-    # remove features that have high number of null data confirm with
-    # nulls = data.isna().sum()
-    # print(data[data > 0])
-    cols_to_drop = ['BsmtFin_SF_2', 'Misc_Val', 'Mo_Sold', 'Three_season_porch',
-       'Year_Sold', 'Bsmt_Half_Bath', 'Low_Qual_Fin_SF', 'Pool_Area']
-    X_train = X_train.drop(columns=cols_to_drop)
-    X_test = X_test.drop(columns=cols_to_drop)
-
-    X_train = add_feature_engineered_cols(X_train)
-    X_test = add_feature_engineered_cols(X_test)
-
-    numeric_cols = X_train.select_dtypes(include=[np.number]).columns
-    categorical_cols = X_train.select_dtypes(include=['object', 'category']).columns
-
-    X_train_numeric = clean_and_process_numeric_features(X_train[numeric_cols])
-    X_test_numeric = clean_and_process_numeric_features(X_test[numeric_cols])
-
-    X_train_cat, X_test_cat = clean_and_process_categorical_features(X_train[categorical_cols], X_test[categorical_cols])
-
-    X_train = pd.concat([X_train_numeric, X_train_cat], axis=1)
-    X_test = pd.concat([X_test_numeric, X_test_cat], axis=1)
-
-    return X_train, y_train, X_test
-
-def fit_ridge(X_train, y_train, X_test, alpha=1.0):
-
+def fit_ridge(X, y, alpha=1.0):
     ridge_model = Ridge(alpha=alpha)
-    ridge_model.fit(X_train, y_train)
-    
-    return ridge_model.predict(X_test)
+    ridge_model.fit(X, y)
+    return ridge_model
 
-def fit_tree(X_train, y_train, X_test):
-    #TODO
-    tree_model = DecisionTreeRegressor(X_train, y_train)
-    return tree_model.predict(X_test) 
 
-def save_predictions(predictions, filename):
+def fit_tree(X, y):
+    # Following instructions here - https://campuswire.com/c/GB46E5679/feed/327
+    np.random.seed(1735)
+    model = XGBRegressor(max_depth=6, learning_rate=0.05, n_estimators=5000, subsample=0.5)
+    model.fit(X, y)
+    return model
+
+
+def preprocess_train_data(train_data):
+    X = train_data.drop(columns=["Sale_Price", "PID"])
+    y = train_data.Sale_Price.apply(np.log)
+
+    # Following instructions here - https://campuswire.com/c/GB46E5679/feed/326
+    X['Garage_Yr_Blt'] = X['Garage_Yr_Blt'].fillna(0)
+
+    num_cols = X.select_dtypes(include=[np.number]).columns
+    cat_cols = X.select_dtypes(include=['object', 'category']).columns
+
+    scaler = StandardScaler()
+    scaler.fit(X[num_cols])
+
+    ohe = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+    ohe.fit(X[cat_cols])
+
+    X = pd.concat(
+        [
+            pd.DataFrame(scaler.transform(X[num_cols]), columns=num_cols),
+            pd.DataFrame(ohe.transform(X[cat_cols]), columns=ohe.get_feature_names_out(cat_cols))
+        ],
+        axis=1
+    )
+
+    return X, y, num_cols, cat_cols, scaler, ohe
+
+
+def preprocess_test_data(test_data, num_cols, cat_cols, scaler, ohe):
+    X = test_data.drop(columns=["PID"])
+
+    # Following instructions here - https://campuswire.com/c/GB46E5679/feed/326
+    X['Garage_Yr_Blt'] = X['Garage_Yr_Blt'].fillna(0)
+
+    X = pd.concat(
+        [
+            pd.DataFrame(scaler.transform(X[num_cols]), columns=num_cols),
+            pd.DataFrame(ohe.transform(X[cat_cols]), columns=ohe.get_feature_names_out(cat_cols))
+        ],
+        axis=1
+    )
+
+    return X, test_data.PID
+
+
+def save_predictions(pid, predictions, filename):
     """
     Save predictions to local file
     """
-    pd.DataFrame(predictions, columns=["predictions"]).to_csv(filename, index=False)
+    pd.DataFrame({'PID': pid, 'Sale_Price': predictions}).to_csv(filename, index=False, header=True)
 
-def main(train_file, test_file):
-    # Step 1: Load data
-    train_data, test_data = load_data(train_file, test_file)
-    
-    # Step 2: Preprocess the train and test data
-    X_train, y_train, X_test = preprocess_data(train_data, test_data)
-    
-    linear_predictions = fit_ridge(X_train, y_train, X_test, alpha=1.0)
-    #tree_predictions = fit_tree(X_train, y_train, X_test)
 
-    save_predictions(linear_predictions, "mysubmission1.txt")
-    #save_predictions(tree_predictions, "mysubmission2.txt")
+def main(train_file, test_file, linear_pred_file, tree_pred_file):
+    # Step 1: Preprocess the training data, then fit the two models.
+    train_data = pd.read_csv(train_file)
+    X, y, num_cols, cat_cols, scaler, ohe = preprocess_train_data(train_data)
+    # linear_model = fit_ridge(X, y)
+    tree_model = fit_tree(X, y)
+
+    # Step 2: Preprocess test data, then save predictions into two files: mysubmission1.txt and mysubmission2.txt
+    test_data = pd.read_csv(test_file)
+    X, pid = preprocess_test_data(test_data, num_cols, cat_cols, scaler, ohe)
+    # linear_pred = linear_model.predict(X)
+    tree_pred = tree_model.predict(X)
+
+    # save_predictions(pid, np.exp(linear_pred), linear_pred_file)
+    save_predictions(pid, np.exp(tree_pred), tree_pred_file)
+
+    # TODO: Delete before submitting
+    return compute_rmse(linear_pred_file, tree_pred_file)
+
+def compute_rmse(linear_pred_file, tree_pred_file):
+    actual = pd.read_csv(join(dirname(linear_pred_file), 'test_y.csv'))
+    y_actual = actual.Sale_Price.apply(np.log).to_numpy()
+
+    def rmse(pred_file):
+        pred = pd.read_csv(pred_file)
+        y_pred = pred.Sale_Price.apply(np.log).to_numpy()
+        assert np.all(actual.PID == pred.PID)
+        return np.sqrt(np.mean((y_pred - y_actual) ** 2))
+
+    return np.inf, rmse(tree_pred_file)
+
 
 if __name__ == "__main__":
-    
-    train_file = "./train.csv"
-    test_file = "./test.csv"
-    
-    main(train_file, test_file)
+    # TODO: Enable before submitting
+    # main('./train.csv', './test.csv', './mysubmission1.txt', './mysubmission2.txt')
+
+
+    # TODO: Delete before submitting
+    for fold in range(1, 11):
+        fold_dir = f'./project_1/proj1/fold{fold}'
+        t1 = time.time()
+        linear_rmse, tree_rmse = main(join(fold_dir, 'train.csv'), join(fold_dir, 'test.csv'), join(fold_dir, 'mysubmission1.txt'), join(fold_dir, 'mysubmission2.txt'))
+        duration = time.time() - t1
+        print(f"Fold {fold} ({fold_dir}) - rmse(linear): {linear_rmse} rmse(tree): {tree_rmse} - {duration:.2f} s")
