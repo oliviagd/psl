488fbfd588255f3199425d5291cbcaf835fe604c
oliviagd <dalglisholivia@gmail.com>
2024-11-18 23:44:19 +0100

    add simple3

---
+++ project2/mymain_simple3.py
@@ -0,0 +1,87 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+from sklearn.decomposition import TruncatedSVD
+
+
+def add_date_features(df):
+    df["Date"] = pd.to_datetime(df["Date"])
+    df["Year"] = df["Date"].dt.year
+    df["Week"] = df["Date"].dt.isocalendar().week
+
+    return df
+
+
+def apply_svd_by_department(train, test, k=8):
+    id_cols = ["Store", "Dept"]
+    train = pd.merge(train, test[id_cols], how="inner")
+    test = pd.merge(test, train[id_cols], how="inner")
+
+    train = pd.concat(
+        [train, pd.get_dummies(train["Dept"], prefix="Dept", dtype=int)], axis=1
+    )
+    test = pd.concat(
+        [test, pd.get_dummies(test["Dept"], prefix="Dept", dtype=int)], axis=1
+    )
+
+    svd_columns = [f"svd_{i}" for i in range(k)]
+    svd = TruncatedSVD(n_components=k)
+
+    dept_cols = [col for col in train.columns if "Dept_" in col]
+
+    X_train_reduced = svd.fit_transform(train[dept_cols])
+    X_train_reduced = pd.DataFrame(X_train_reduced, columns=svd_columns)
+
+    X_test_reduced = svd.transform(test[dept_cols])
+    X_test_reduced = pd.DataFrame(X_test_reduced, columns=svd_columns)
+
+    train = train.drop(columns=dept_cols).reset_index(drop=True)
+    test = test.drop(columns=dept_cols).reset_index(drop=True)
+
+    return pd.concat([train, X_train_reduced], axis=1), pd.concat(
+        [test, X_test_reduced], axis=1
+    )
+
+
+if __name__ == "__main__":
+    test_with_label = pd.read_csv(
+        "/Users/oliviadalglish/UIUC/tmp/psl/project2/Proj2_Data/test_with_label.csv"
+    )
+
+    train = pd.read_csv(f"train.csv")
+    test = pd.read_csv(f"test.csv")
+    test = pd.merge(
+        test, test_with_label, on=["Store", "Dept", "Date", "IsHoliday"], how="left"
+    )
+
+    train = add_date_features(train)
+    test = add_date_features(test)
+
+    preds = []
+    for store in train["Store"].unique():
+        train_tmp = train[train["Store"] == store]
+        test_tmp = test[test["Store"] == store]
+        train_tmp, test_tmp = apply_svd_by_department(train_tmp, test_tmp)
+        
+        model = LinearRegression()
+        model.fit(
+            train_tmp.drop(columns=["Weekly_Sales", "Dept", "Date"]),
+            train_tmp.Weekly_Sales,
+        )
+
+        # predict and calculate error
+        test_tmp["Weekly_Sales_Pred"] = model.predict(
+            test_tmp.drop(columns=["Weekly_Sales", "Dept", "Date"])
+        )
+        preds.append(test_tmp)
+
+    all_preds = pd.concat(preds)
+    all_preds["weight"] = all_preds["IsHoliday"].apply(lambda x: 5 if x else 1)
+
+    print(
+        mean_absolute_error(
+            all_preds["Weekly_Sales"],
+            all_preds["Weekly_Sales_Pred"],
+            sample_weight=all_preds["weight"],
+        )
+    )
--- project2/runner.py
+++ project2/runner.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 from tqdm import tqdm
 
@@ -19,13 +20,13 @@ def run_fold(fold_data, code_path):
     print(f"Execution time for {fold_data}: {execution_time:.2f} seconds")
 
 
-def main():
+def main(code_script="mymain.py"):
     folds = [f"fold_{i}" for i in range(1, 11)]
 
     cwd = os.getcwd()
 
     project_data_path = os.path.join(cwd, "Proj2_Data")
-    code_path = os.path.join(cwd, "mymain.py")
+    code_path = os.path.join(cwd, code_script)
 
     for fold in tqdm(folds, desc="Running Project 2"):
         fold_data = os.path.join(project_data_path, fold)
@@ -38,4 +39,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    code_script = "mymain.py" if len(sys.argv) < 1 else sys.argv[1]
+    main(code_script=code_script)
