4f377fd8d0911fe72ed8a59e8385fefb1a4cad53
oliviagd <dalglisholivia@gmail.com>
2024-11-18 18:20:45 +0100

    add svd by department

--- project2/mymain.py
+++ project2/mymain.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 import numpy as np
 from sklearn.metrics import mean_absolute_error
 from sklearn.linear_model import LinearRegression
+from sklearn.decomposition import TruncatedSVD
+from sklearn.preprocessing import StandardScaler
 
 
 def add_date_features(df):
@@ -12,6 +14,30 @@ def add_date_features(df):
     df["Week"] = df["Date"].dt.isocalendar().week
 
     return df
+
+
+def apply_svd_by_department(train, test, k=8):
+    svd_columns = [f"svd_{i}" for i in range(k)]
+    svd = TruncatedSVD(n_components=k)
+
+    dept_cols = [col for col in X_train.columns if "Dept_" in col]
+
+    X_train_reduced = svd.fit_transform(train[dept_cols])
+    X_train_reduced = pd.DataFrame(X_train_reduced, columns=svd_columns)
+
+    X_test_reduced = svd.transform(test[dept_cols])
+    X_test_reduced = pd.DataFrame(X_test_reduced, columns=svd_columns)
+
+    return (
+        pd.concat(
+            [train.drop(columns=dept_cols).reset_index(drop=True), X_train_reduced],
+            axis=1,
+        ),
+        pd.concat(
+            [test.drop(columns=dept_cols).reset_index(drop=True), X_test_reduced],
+            axis=1,
+        ),
+    )
 
 
 if __name__ == "__main__":
@@ -38,11 +64,14 @@ if __name__ == "__main__":
     filtered_df = df[valid_groups]
 
     # split up train data by store so that we can fit a model separately for
-    # each store
+    # each store. list will hold tuples of (train, test) data for each store
     data_by_store = []
 
     for store in filtered_df.Store.unique():
+        # filter data by store
         df_tmp = filtered_df[filtered_df["Store"] == store]
+
+        # one-hot encode department variable
         dummies = pd.get_dummies(df_tmp["Dept"], prefix="Dept", dtype=int)
         df_tmp_w_categorical = pd.concat([df_tmp, dummies], axis=1)
         train_tmp = df_tmp_w_categorical[df_tmp_w_categorical["istrain"] == 1].drop(
@@ -56,19 +85,21 @@ if __name__ == "__main__":
 
     errors, preds = [], []
     for store_train, store_test in data_by_store:
-        X = store_train.drop(columns=["Weekly_Sales"])
-        y = store_train["Weekly_Sales"]
+        X_train = store_train.drop(columns=["Weekly_Sales"])
+        y_train = store_train["Weekly_Sales"]
+
+        X_train, X_test = apply_svd_by_department(X_train, store_test)
 
         # fit model
         model = LinearRegression()
-        model.fit(X.drop(columns=["Dept", "Date"]), y)
+        model.fit(X_train.drop(columns=["Dept", "Date"]), y_train)
 
         # predict and calculate error
-        y_pred = model.predict(store_test.drop(columns=["Dept", "Date"]))
-        store_test["Weekly_Pred"] = y_pred
+        y_pred = model.predict(X_test.drop(columns=["Dept", "Date"]))
+        X_test["Weekly_Pred"] = y_pred
 
         # append store-predictions
-        store_preds = store_test[["Store", "Dept", "Date", "IsHoliday", "Weekly_Pred"]]
+        store_preds = X_test[["Store", "Dept", "Date", "IsHoliday", "Weekly_Pred"]]
         preds.append(store_preds)
         y_true = pd.merge(
             results, store_preds, on=["Store", "Dept", "Date", "IsHoliday"]
