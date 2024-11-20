c21932d7f7ba75a32bc53e16c8bd5348bf72e675
oliviagd <dalglisholivia@gmail.com>
2024-11-20 13:34:55 +0100

    rm

--- project2/mymain_simple8.py
+++ project2/mymain_simple8.py
@@ -6,52 +6,12 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.preprocessing import OneHotEncoder
 pd.options.mode.chained_assignment = None  # Disable warning
 
-def identify_sales_bulge(data):
-    # Filter relevant weeks
-    holiday_weeks = data[data['Week'].isin([48, 49, 50, 51, 52])]
-    
-    # Calculate average weekly sales per department
-    avg_sales = holiday_weeks.groupby(['Dept', 'Week'])['Weekly_Sales'].mean().unstack()
-    
-    # Calculate averages for specific week ranges
-    avg_49_51 = avg_sales[[49, 50, 51]].mean(axis=1)
-    avg_48_52 = avg_sales[[48, 52]].mean(axis=1)
-    
-    # Identify departments with a sales bulge
-    sales_bulge_depts = avg_sales.index[avg_49_51 >= 1.1 * avg_48_52]
-    
-    return sales_bulge_depts
-
-def adjust_christmas_sales(data, sales_bulge_depts, fraction):
-    # Copy data to avoid modifying the original
-    adjusted_data = data.copy()
-    
-    # Filter for holiday weeks and sales bulge departments
-    holiday_weeks = adjusted_data['Week'].isin([48, 49, 50, 51, 52])
-    bulge_depts = adjusted_data['Dept'].isin(sales_bulge_depts)
-    
-    # Apply sales adjustment
-    for dept in sales_bulge_depts:
-        for wk in [48, 49, 50, 51, 52]:
-            current_week_mask = (adjusted_data['Dept'] == dept) & (adjusted_data['Week'] == wk)
-            next_week = 48 if wk == 52 else wk + 1  # Wrap week 52 to week 48
-            
-            next_week_mask = (adjusted_data['Dept'] == dept) & (adjusted_data['Week'] == next_week)
-            
-            # Shift sales
-            adjusted_data.loc[next_week_mask, 'Weekly_Sales'] += (
-                fraction * adjusted_data.loc[current_week_mask, 'Weekly_Sales'].sum()
-            )
-            adjusted_data.loc[current_week_mask, 'Weekly_Sales'] *= (1 - fraction)
-    
-    return adjusted_data
 
 def add_date_features(df):
     df["Date"] = pd.to_datetime(df["Date"])
     df["Year"] = df["Date"].dt.year
-    df["Week"] = df["Date"].dt.isocalendar().week
+    df["Week"] = df["Date"].dt.isocalendar().week.astype(int)
     df["Year^2"] = df["Year"] ** 2 
-    #df.loc[df['Year'] == 2010, 'Week'] -= 1
     
     return df
 
@@ -92,7 +52,11 @@ def svd_dept(train_data, num_components=8):
         
         smoothed_data.append(smoothed_df)
     
-    return pd.concat(smoothed_data, ignore_index=True)
+    
+    df = pd.concat(smoothed_data, ignore_index=True)
+    df['Store'] = df['Store'].astype(int)
+    df['Dept'] = df['Dept'].astype(int)
+    return df
 
 def fit_and_predict(train_data, smoothed_data, test_data):
     train_data = train_data.merge(smoothed_data, on=['Date', 'Store', 'Dept'])
@@ -105,15 +69,10 @@ def fit_and_predict(train_data, smoothed_data, test_data):
     for store, dept in train_data[['Store', 'Dept']].drop_duplicates().values:
         train_tmp = train_data.query(f"(Dept == {dept}) & (Store == {store})")
         test_tmp = test_data.query(f"(Dept == {dept}) & (Store == {store})")
-        
+
         X_train = train_tmp.drop(columns=["Date", "Weekly_Sales"])
         y_train = train_tmp['Weekly_Sales']
-        
         X_test = test_tmp.drop(columns=["Date", "Weekly_Sales"])
-        
-        #poly = PolynomialFeatures(degree=2, include_bias=False)
-        #X_train = poly.fit_transform(X_train)
-        #X_test = poly.transform(X_test)
         
         model = LinearRegression()
         model.fit(X_train, y_train)
@@ -125,30 +84,23 @@ def fit_and_predict(train_data, smoothed_data, test_data):
 
 def apply_one_hot(train, test, cat_cols):
     ohe = OneHotEncoder(
-        sparse_output=False, handle_unknown="ignore", min_frequency=0.01
+        sparse_output=False, handle_unknown="ignore"
     )
     ohe.fit(train[cat_cols])
 
-    train = pd.concat(
-        [
-            train,
-            pd.DataFrame(
-                ohe.transform(train[cat_cols]),
-                columns=ohe.get_feature_names_out(cat_cols),
-            ),
-        ],
-        axis=1,
+    train_ohe = pd.DataFrame(
+        ohe.transform(train[cat_cols]),
+        columns=ohe.get_feature_names_out(cat_cols),
+        index=train.index,
     )
-    test = pd.concat(
-        [
-            test,
-            pd.DataFrame(
-                ohe.transform(test[cat_cols]),
-                columns=ohe.get_feature_names_out(cat_cols),
-            ),
-        ],
-        axis=1,
+    test_ohe = pd.DataFrame(
+        ohe.transform(test[cat_cols]),
+        columns=ohe.get_feature_names_out(cat_cols),
+        index=test.index,
     )
+
+    train = pd.concat([train, train_ohe], axis=1)
+    test = pd.concat([test, test_ohe], axis=1)
 
     return train, test
 
